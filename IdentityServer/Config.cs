0d84fb77fc119ea32abc83a2589d4b1af8daf59f
Usman Tahir <Usman.tahir@synergy-it.com>
2023-12-29 16:16:51 +0500

    Intergrate openidconnect

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,5 +1,9 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
 using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace IdentityServer
 {
@@ -18,36 +22,36 @@ namespace IdentityServer
                         },
                         AllowedScopes = { "movieAPI" }
                    }
-                   //,
-                   //new Client
-                   //{
-                   //    ClientId = "movies_mvc_client",
-                   //    ClientName = "Movies MVC Web App",
-                   //    AllowedGrantTypes = GrantTypes.Hybrid,
-                   //    RequirePkce = false,
-                   //    AllowRememberConsent = false,
-                   //    RedirectUris = new List<string>()
-                   //    {
-                   //        "https://localhost:5002/signin-oidc"
-                   //    },
-                   //    PostLogoutRedirectUris = new List<string>()
-                   //    {
-                   //        "https://localhost:5002/signout-callback-oidc"
-                   //    },
-                   //    ClientSecrets = new List<Secret>
-                   //    {
-                   //        new Secret("secret".Sha256())
-                   //    },
-                   //    AllowedScopes = new List<string>
-                   //    {
-                   //        IdentityServerConstants.StandardScopes.OpenId,
-                   //        IdentityServerConstants.StandardScopes.Profile,
-                   //        IdentityServerConstants.StandardScopes.Address,
-                   //        IdentityServerConstants.StandardScopes.Email,
-                   //        "movieAPI",
-                   //        "roles"
-                   //    }
-                   //}
+                   ,
+                   new Client
+                   {
+                       ClientId = "movies_mvc_client",
+                       ClientName = "Movies MVC Web App",
+                       AllowedGrantTypes = GrantTypes.Hybrid,
+                       RequirePkce = false,
+                       AllowRememberConsent = false,
+                       RedirectUris = new List<string>()
+                       {
+                           "https://localhost:5002/signin-oidc"
+                       },
+                       PostLogoutRedirectUris = new List<string>()
+                       {
+                           "https://localhost:5002/signout-callback-oidc"
+                       },
+                       ClientSecrets = new List<Secret>
+                       {
+                           new Secret("secret".Sha256())
+                       },
+                       AllowedScopes = new List<string>
+                       {
+                           IdentityServerConstants.StandardScopes.OpenId,
+                           IdentityServerConstants.StandardScopes.Profile,
+                           IdentityServerConstants.StandardScopes.Address,
+                           IdentityServerConstants.StandardScopes.Email,
+                           "movieAPI",
+                           "roles"
+                       }
+                   }
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -56,39 +60,39 @@ namespace IdentityServer
                new ApiScope("movieAPI", "Movie API")
            };
 
-        //public static IEnumerable<ApiResource> ApiResources =>
-        //  new ApiResource[]
-        //  {
-        //       //new ApiResource("movieAPI", "Movie API")
-        //  };
+        public static IEnumerable<ApiResource> ApiResources =>
+          new ApiResource[]
+          {
+               new ApiResource("movieAPI", "Movie API")
+          };
 
-        //public static IEnumerable<IdentityResource> IdentityResources =>
-        //  new IdentityResource[]
-        //  {
-        //      new IdentityResources.OpenId(),
-        //      new IdentityResources.Profile(),
-        //      new IdentityResources.Address(),
-        //      new IdentityResources.Email(),
-        //      new IdentityResource(
-        //            "roles",
-        //            "Your role(s)",
-        //            new List<string>() { "role" })
-        //  };
+        public static IEnumerable<IdentityResource> IdentityResources =>
+          new IdentityResource[]
+          {
+              new IdentityResources.OpenId(),
+              new IdentityResources.Profile(),
+              new IdentityResources.Address(),
+              new IdentityResources.Email(),
+              new IdentityResource(
+                    "roles",
+                    "Your role(s)",
+                    new List<string>() { "role" })
+          };
 
-        //public static List<TestUser> TestUsers =>
-        //    new List<TestUser>
-        //    {
-        //        new TestUser
-        //        {
-        //            SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
-        //            Username = "usman",
-        //            Password = "usman",
-        //            Claims = new List<Claim>
-        //            {
-        //                new Claim(JwtClaimTypes.GivenName, "usman"),
-        //                new Claim(JwtClaimTypes.FamilyName, "tahir")
-        //            }
-        //        }
-        //    };
+        public static List<TestUser> TestUsers =>
+            new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "usman",
+                    Password = "usman",
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "usman"),
+                        new Claim(JwtClaimTypes.FamilyName, "tahir")
+                    }
+                }
+            };
     }
 }
---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,39 @@
+﻿using Movies.Client.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> GetMovies()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<UserInfoViewModel> GetUserInfo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
