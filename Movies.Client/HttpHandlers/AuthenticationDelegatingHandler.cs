bcee774dd83c652d8f7234f10f5cb57bcc165227
Usman Tahir <Usman.tahir@synergy-it.com>
2024-03-13 22:04:11 +0500

    Developing HttpHandler for Getting Token from IDPClient

--- Movies.Client/HttpHandlers/AuthenticationDelegatingHandler.cs
+++ Movies.Client/HttpHandlers/AuthenticationDelegatingHandler.cs
@@ -1,8 +1,33 @@
-﻿using System.Net.Http;
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Movies.Client.HttpHandlers
 {
     public class AuthenticationDelegatingHandler : DelegatingHandler
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ClientCredentialsTokenRequest _tokenRequest;
+
+        public AuthenticationDelegatingHandler(IHttpClientFactory httpClientFactory, ClientCredentialsTokenRequest tokenRequest)
+        {
+            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+            _tokenRequest = tokenRequest ?? throw new ArgumentNullException(nameof(tokenRequest));
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var httpclient = _httpClientFactory.CreateClient("idpclient");
+
+            var tokenresponse = await httpclient.RequestClientCredentialsTokenAsync(_tokenRequest);
+            if (tokenresponse.IsError)
+            {
+                throw new HttpRequestException("something went wrong while requesting the access token");
+            }
+            request.SetBearerToken(tokenresponse.AccessToken);
+            return await base.SendAsync(request, cancellationToken);
+        }
     }
 }
--- Movies.Client/Startup.cs
+++ Movies.Client/Startup.cs
@@ -12,6 +12,7 @@ using Movies.Client.ApiServices;
 using Microsoft.Net.Http.Headers;
 using Movies.Client.HttpHandlers;
 using System;
+using IdentityModel.Client;
 
 namespace Movies.Client
 {
@@ -69,10 +70,29 @@ namespace Movies.Client
 
             services.AddHttpClient("MovieAPIClient", client =>
             {
-                client.BaseAddress = new Uri("https://localhost:5010/"); // API GATEWAY URL
+                client.BaseAddress = new Uri("https://localhost:5001/"); // API GATEWAY URL
                 client.DefaultRequestHeaders.Clear();
                 client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
             }).AddHttpMessageHandler<AuthenticationDelegatingHandler>();
+
+
+            // 2 create an HttpClient used for accessing the IDP
+            services.AddHttpClient("IDPClient", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:5005/");
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+            });
+
+            services.AddHttpContextAccessor();
+
+            services.AddSingleton(new ClientCredentialsTokenRequest
+            {
+                Address = "https://localhost:5005/connect/token",
+                ClientId = "movieClient",
+                ClientSecret = "secret",
+                Scope = "movieAPI"
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
